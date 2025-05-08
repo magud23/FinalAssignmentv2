480c791da4765254f1592652f96a9aa543214c7b
magud23 <162568762+magud23@users.noreply.github.com>
2025-05-08 21:16:40 +0200

    Added LCD (not tested)

---
+++ lcd.h
@@ -0,0 +1,82 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Embedded C Programming (ECP)
+*
+* MODULENAME.: leds.h
+*
+* PROJECT....: ECP
+*
+* DESCRIPTION: Test.
+*
+* Change Log:
+******************************************************************************
+* Date    Id    Change
+* YYMMDD
+* --------------------
+* 050128  KA    Module created.
+* 250508  MKG   Handled magic numbers
+*
+*****************************************************************************/
+
+#ifndef _LCD_H
+  #define _LCD_H
+
+/***************************** Include files *******************************/
+
+/*****************************    Defines    *******************************/
+// Special ASCII characters
+// ------------------------
+
+#define LF		0x0A
+#define FF		0x0C
+#define CR		0x0D
+
+#define ESC		0x1B
+#define LCD_RS  0x04
+#define LCD_E   0x08
+#define LSB4    0x0F
+#define MSB4    0xF0
+#define SET_DRAM_ADDR 0x80
+#define SECOND_LINE_ADDR 0x40
+
+
+/*****************************   Constants   *******************************/
+
+/*****************************   Functions   *******************************/
+void wr_str_LCD( INT8U *pStr );
+/*****************************************************************************
+*   Function : See module specification (.h-file).
+*****************************************************************************/
+
+
+void move_LCD( INT8U, INT8U );
+/*****************************************************************************
+*   Function : See module specification (.h-file).
+*****************************************************************************/
+
+INT8U wr_ch_LCD( INT8U Ch );
+/*****************************************************************************
+*   OBSERVE  : LCD_PROC NEEDS 20 mS TO PRINT OUT ONE CHARACTER
+*   Function : See module specification (.h-file).
+*****************************************************************************/
+
+void home_LCD();
+/*****************************************************************************
+*   Input    : -
+*   Output   : -
+*   Function : Return cursor to the home position.
+******************************************************************************/
+
+void lcd_task( void *pvParameters );
+/*****************************************************************************
+*   Input    : -
+*   Output   : -
+*   Function : Handles the output to LCD, Access using QueueHandle_t xQueue_lcd
+*   >>ESC,@<< to home
+*   >>ESC (0x80 | cursor pos)<< to position cursor
+*   >>0xFF<< to clear
+******************************************************************************/
+
+/****************************** End Of Module *******************************/
+#endif
+
--- main.c
+++ main.c
@@ -18,28 +18,55 @@
 
 /***************************** Include files *******************************/
 #include <stdint.h>
+//FreeRTOS
 #include "FreeRTOS.h"
-#include "tm4c123gh6pm.h"
-#include "emp_type.h"
 #include "systick_frt.h"
 #include "task.h"
-#include "status_led.h"
+#include "semphr.h"
 #include "queue.h"
+
+//Course
+#include "tm4c123gh6pm.h"
+#include "emp_type.h"
+
+//Modules
+#include "glob_def.h"
+#include "status_led.h"
 #include "uart.h"
 #include "adcRTOS.h"
 #include "leds.h"
+#include "lcd.h"
+
+
 
 /*****************************    Defines    *******************************/
-/*****************************   Constants   *******************************/
-/*****************************   Variables   *******************************/
-
-
-/*****************************   Functions   *******************************/
+//task
 #define USERTASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define IDLE_PRIO 0
 #define LOW_PRIO  1
 #define MED_PRIO  2
 #define HIGH_PRIO 3
+#define QUEUE_LEN   128
+#define BUFFER_LEN 1
+
+//UART
+#define BAUDRATE 9600
+#define DATABITS 8
+#define STOPBITS 1
+#define PARITY 'n'
+
+
+/*****************************   Constants   *******************************/
+/*****************************   Variables   *******************************/
+extern QueueHandle_t uart_rx_q;
+extern QueueHandle_t uart_tx_q;
+extern QueueHandle_t adc_q;
+extern QueueHandle_t xQueue_lcd;
+
+extern SemaphoreHandle_t xSemaphore_lcd;
+
+
+/*****************************   Functions   *******************************/
 
 
 static void setupHardware(void)
@@ -53,32 +80,48 @@ static void setupHardware(void)
   // Warning: If you do not initialize the hardware clock, the timings will be inaccurate
   init_systick();
   status_led_init();
-  uart0_init(9600,8,1,'n');
+  uart0_init(BAUDRATE,DATABITS,STOPBITS, PARITY);
 
 }
 
+static INT16U setupInterTaskCommunication(void)
+/*****************************************************************************
+*   Input    :  -
+*   Output   :  -
+*   Function :
+*****************************************************************************/
+{
+     INT16U tmp = TRUE;
+     uart_rx_q = xQueueCreate(QUEUE_LEN, sizeof(INT8U));
+     uart_tx_q = xQueueCreate(QUEUE_LEN, sizeof(INT8U));
+     tmp = (uart_rx_q != NULL) && (uart_tx_q != NULL);
+
+     adc_q = xQueueCreate(BUFFER_LEN, sizeof(INT16U));
+     tmp = tmp && ( adc_q != NULL);
+
+     xQueue_lcd = xQueueCreate( QUEUE_LEN , sizeof( INT8U ));
+     tmp = tmp && ( xQueue_lcd != NULL);
+
+     xSemaphore_lcd = xSemaphoreCreateMutex();
+     tmp = tmp && ( xSemaphore_lcd != NULL);
 
 
+     return tmp;
+}
 
-extern QueueHandle_t uart_rx_q;
-extern QueueHandle_t uart_tx_q;
-extern QueueHandle_t adc_q;
 
 
 int main(void)
 {
     setupHardware();
-
-    uart_rx_q = xQueueCreate(128, sizeof(INT8U));
-    uart_tx_q = xQueueCreate(128, sizeof(INT8U));
-    adc_q = xQueueCreate(1, sizeof(INT16U));
-
-    if( (uart_rx_q != NULL) && (uart_tx_q != NULL) ){
+    if(setupInterTaskCommunication()){
            xTaskCreate( status_led_task, "Status_led", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
            xTaskCreate( uart_rx_task, "Uart rx", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
            xTaskCreate( uart_tx_task, "Uart tx", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
            xTaskCreate( adc_task, "ADC (potmeter)", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
-           xTaskCreate( leds_task, "ADC (potmeter)", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
+           xTaskCreate( leds_task, "LEDS", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
+           xTaskCreate( lcd_task, "LCD driver", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
+
            vTaskStartScheduler();
     }
 
